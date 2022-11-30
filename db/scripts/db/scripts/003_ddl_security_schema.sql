f9a5c3cb62036f62fc84bcf447f1111ec2bff9b0
Bitok85 <kan041185@gmail.com>
2022-11-30 12:28:38 +0300

    1. Авторизация через JDBC [#2094]

--- db/scripts/db/scripts/003_ddl_security_schema.sql
+++ db/scripts/db/scripts/003_ddl_security_schema.sql
@@ -1,13 +1,14 @@
 CREATE TABLE users (
    id SERIAL PRIMARY KEY,
-   username TEXT NOT NULL,
+   username TEXT NOT NULL UNIQUE ,
    password TEXT NOT NULL,
    enabled boolean default true
 );
 
 CREATE TABLE authorities (
-     username TEXT NOT NULL,
+     username TEXT NOT NULL UNIQUE ,
      authority TEXT NOT NULL,
      user_id int REFERENCES users(id)
 );
 
+select * from authorities;
