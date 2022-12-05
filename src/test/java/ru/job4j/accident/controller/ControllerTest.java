a622af6a2df070f5e62d9dc39280dc2057596641
Bitok85 <kan041185@gmail.com>
2022-12-05 13:02:47 +0300

    0. Spring test [#6881]

--- src/test/java/ru/job4j/accident/controller/ControllerTest.java
+++ src/test/java/ru/job4j/accident/controller/ControllerTest.java
@@ -4,18 +4,40 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.job4j.accident.App;
+import ru.job4j.accident.model.Accident;
+import ru.job4j.accident.model.User;
+import ru.job4j.accident.service.AccidentService;
+import org.mockito.ArgumentCaptor;
+import ru.job4j.accident.service.UserService;
+
+import static org.assertj.core.api.Assertions.*;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.mockito.Mockito.verify;
 
 @SpringBootTest(classes = App.class)
+@ActiveProfiles("test")
 @AutoConfigureMockMvc
+@TestPropertySource("classpath:application-test.properties")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public class ControllerTest {
+
+    @MockBean
+    private AccidentService accidentService;
+
+    @MockBean
+    private UserService userService;
 
     @Autowired
     private MockMvc mockMvc;
@@ -54,6 +76,56 @@ public class ControllerTest {
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(view().name("reg"));
+    }
+
+    @Test
+    @WithMockUser
+    public void whenCreateAccidentThenReturnDefaultMessage() throws Exception {
+        this.mockMvc.perform(post("/createAccident")
+                .param("id", "1")
+                .param("name", "Машина-мотоцикл")
+                .param("text", "some text")
+                .param("address", "MSK")
+                .param("type.id", "1")
+                .param("ruleIds", "1"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/index"));
+        ArgumentCaptor<Accident> argument = ArgumentCaptor.forClass(Accident.class);
+        verify(accidentService).addAccident(argument.capture());
+        assertThat(argument.getValue().getAddress()).isEqualTo("MSK");
+    }
+
+    @Test
+    @WithMockUser
+    public void whenEditAccidentThenReturnDefaultMessage() throws Exception {
+        this.mockMvc.perform(post("/editAccident")
+                        .param("id", "1")
+                        .param("name", "Машина-мотоцикл")
+                        .param("text", "some text")
+                        .param("address", "MSK")
+                        .param("type.id", "1")
+                        .param("ruleIds", "1"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/index"));
+        ArgumentCaptor<Accident> argument = ArgumentCaptor.forClass(Accident.class);
+        verify(accidentService).editAccident(argument.capture());
+        assertThat(argument.getValue().getAddress()).isEqualTo("MSK");
+    }
+
+    @Test
+    @WithMockUser
+    public void whenRegUserThenReturnDefaultMessage() throws Exception {
+        this.mockMvc.perform(post("/reg")
+                .param("username", "name")
+                .param("password", "123"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/login"));
+        ArgumentCaptor<User> argument = ArgumentCaptor.forClass(User.class);
+        verify(userService).save(argument.capture());
+        assertThat(argument.getValue().getUsername()).isEqualTo("name");
     }
 
 }
