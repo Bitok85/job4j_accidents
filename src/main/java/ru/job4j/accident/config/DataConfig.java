316814ecb987723d250f950ec14166765d6cd848
Bitok85 <kan041185@gmail.com>
2022-12-05 12:58:01 +0300

    0. Spring test [#6881]

--- src/main/java/ru/job4j/accident/config/DataConfig.java
+++ src/main/java/ru/job4j/accident/config/DataConfig.java
@@ -1,12 +1,10 @@
 package ru.job4j.accident.config;
 
-import liquibase.integration.spring.SpringLiquibase;
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
